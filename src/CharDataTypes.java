ff4d1e166c0ce98dcac1b88397c74aea367c7151
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-11 13:00:56 +0700

    learn : char data types

---
+++ src/CharDataTypes.java
@@ -0,0 +1,11 @@
+public class CharDataTypes {
+    public static void main(String[] args) {
+        char a = 'A';
+        char v = 'V';
+        char t = 'T';
+
+        System.out.print(a);
+        System.out.print(v);
+        System.out.print(t);
+    }
+}
