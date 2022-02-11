a431805cb74313227d551c56af2900d15f8fee6b
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-11 13:09:00 +0700

    learn : string data types

---
+++ src/StringDataTypes.java
@@ -0,0 +1,11 @@
+public class StringDataTypes {
+    public static void main(String[] args) {
+        String firstName = "Avtara"; // Default null
+        String lastName = "Khrisna";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+    }
+}
