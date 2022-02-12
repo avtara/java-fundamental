e01d52d797e813cce35c154d12ce2416f3d3b294
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-12 21:58:22 +0700

    learn : ternary operator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,7 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var grade = 80;
+        String res = grade > 75 ? "Graduate" : "Remidial";
+        System.out.println(res);
+    }
+}
