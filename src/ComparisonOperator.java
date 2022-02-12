b552da1214b4afb06652ef9d7e5aec32ed091026
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-12 12:49:32 +0700

    learn : comparison operator

---
+++ src/ComparisonOperator.java
@@ -0,0 +1,13 @@
+public class ComparisonOperator {
+    public static void main(String[] args) {
+        int value1 = 200;
+        int value2 = 200;
+
+        System.out.println(value1 < value2);
+        System.out.println(value1 > value2);
+        System.out.println(value1 <= value2);
+        System.out.println(value1 >= value2);
+        System.out.println(value1 == value2);
+        System.out.println(value1 != value2);
+    }
+}
