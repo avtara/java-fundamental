2f4a9683a54e34d0de91379a881a89baa754a709
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-13 16:32:15 +0700

    learn : do while loop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        var counter = 0;
+        do {
+            System.out.println(counter);
+            counter++;
+        } while (counter < 0);
+    }
+}
