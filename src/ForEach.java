4d43090ac8f1f3f473a3c3f2fd735d0f63263231
Muhammad Avtara Khrisna <avtarakhrisna1@gmail.com>
2022-02-13 16:39:19 +0700

    learn : for each

---
+++ src/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String names[] = {
+                "avtara", "khrisna"
+        };
+
+        for (var name : names){
+            System.out.println(name);
+        }
+    }
+}
